ca5e49b7225f517110e78f7ca545570816412359
Tanous Atallah <tanous.atallah@gmail.com>
2022-03-04 17:07:40 +0100

    finish first stage: no responsive yet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      'black': '#000',
+      'white': '#FFF',
+      'creme': '#feddd8',
+      'gray':'#0000004D'
+    },
+    fontFamily:{
+      'montserrat':''
+    },
+    extend: {
+      backgroundImage:{
+        'hero-image': 'url("/dist/img/bg.webp")',
+        'hero-cards': 'url("/dist/img/cards.webp")',
+        'footer-company': 'url("/dist/img/company.webp")',
+      }
+    },
   },
   plugins: [],
 }
